d166e8da9ade37ab550cf49a79a08afb3fd17025
danalmeidap <dbatistadealmeida23@gmail.com>
2024-04-20 12:28:47 -0300

    Fixing task repository

--- app/tasks.py
+++ app/tasks.py
@@ -1,27 +1,25 @@
 from __future__ import annotations
 from datetime import datetime
 from app.database import mongo
-import pymongo
 from typing import Any
 
 
-def get_all_tasks(active:bool= True) -> list[dict[str, Any]]:
-    tasks = mongo.db.tasks.find({"active": active})
-    return tasks.sort("date", pymongo.DESCENDING)
+def get_all_tasks(active:bool= True) ->dict:
+    tasks = [task for task in mongo.db.tasks.find({"active": active})]
+    return tasks
+                      
 
+def get_tasks_by_user(user:str, active:bool= True) ->dict:
+    user_tasks= [task for task in mongo.db.tasks.find({"user":user})]
+    return user_tasks
 
-def get_tasks_by_user(user:str) ->list[dict[str, Any]]:
-    user_tasks=mongo.db.tasks.find({{"user":user}, {"active":True}})
-    return user_tasks.sort("date", pymongo.DESCENDING)
-
-
-def get_task_by_slug(slug:str) ->dict[str, Any]:
+def get_task_by_slug(slug:str) ->dict:
     task = mongo.db.tasks.find_one({"slug":slug})
     return task
 
 
-def update_task_by_name(user:str, name:str, data:dict[str, Any])->str:
-    return mongo.db.tasks.find_one_and_update({"slug":user}, {"name":name}, {"$set":data})
+def update_task_by_slug(user:str, slug:str, data)->str:
+    return mongo.db.tasks.find_one_and_update({"user":user}, {"slug":slug}, {"$set":data})
 
 
 def new_task(user:str, title:str, content:str, active:bool=True) ->str:
@@ -32,6 +30,7 @@ def new_task(user:str, title:str, content:str, active:bool=True) ->str:
             "title":title,
             "content":content,
             "slug":slug,
+            "active":active,
             "date":datetime.now()
         }
     )
